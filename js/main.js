38d49179537f19abcf1956a92a0fea3fd193bf71
Creed <bonheurjoseph411@gmail.com>
2021-12-05 21:04:50 +0200

    stock class migration

--- js/main.js
+++ js/main.js
@@ -8,75 +8,15 @@
 
 */
 
-class Stock{
-    static NumOfStocks = 0;
-    constructor() {
-        this.AvailableProducts = [];
-        this.productCost = [];
-        this.AvairableNumItems = [];
-        this.NumberOfProducts = 0;
-    };
-            arraySummation = function(arrayName){ //Array should be of the type integer
-                // please find the way to set this mehtod to be the protected member of this class
-                this.counter = 0;
-            for (let i = 0; i < arrayName.length; i++) {
-                this.counter = this.counter + arrayName[i];
-            }
-            
-            return this.counter;
-            }
 
-            addNewProduct = function(product,cost,InitialItems = 1){
-                if ((product !="" && isNaN(product)) && (!isNaN(cost) && cost != 0 )) {
-                    this.AvailableProducts.push(product);
-                    this.productCost.push(cost);
-                    this.AvairableNumItems.push(InitialItems);
-                    this.NumberOfProducts++;
-                    return true;
-                }else{
-                    return false;
-                }
-                                            }
-            
-            pushNewItems = function(ItemIndex,NumberOfItems){
-                this.AvairableNumItems[ItemIndex] = this.AvairableNumItems[ItemIndex] + NumberOfItems;
-                return true;
-            }
-            checkAvailableItems = function(ItemIndex){
-                return this.AvairableNumItems[ItemIndex];
-            }
-
-            popItems = function(ItemIndex,NumberOfItems){
-                if (this.AvairableNumItems[ItemIndex] != 0) {
-                    this.AvairableNumItems[ItemIndex] = this.AvairableNumItems[ItemIndex] - NumberOfItems;
-                    return true;
-                }else{
-                    return false;
-                }
-            }
-
-            updateCost = function(ItemIndex,newCost){
-                this.productCost[ItemIndex] = newCost;
-                return true;
-            }
-
-            totalItemsInStock = function(){
-               return this.arraySummation(this.AvairableNumItems);
-            }
-}
-
-
-
-
-
-
+import{Stock} from './stock.js';
 
 
 //--------------------------------------------testing------------------------------------------
 
 
 var a = new Stock();
-console.log(a.addNewProduct("mango",100));
+console.log(a.addNewProduct("1",100));
 console.log(a.addNewProduct("apple",200));
 
 
--- js/stock.js
+++ js/stock.js
@@ -1,12 +1,60 @@
-// export const Stock = function(){
-//             NumberOfProducts = 0;
-//             AvailableProducts = new array();
-//             AvairableNumItems = new array();
+class Stock{
+    static NumOfStocks = 0;
+    constructor() {
+        this.AvailableProducts = [];
+        this.productCost = [];
+        this.AvairableNumItems = [];
+        this.NumberOfProducts = 0;
+    };
+            arraySummation = function(arrayName){ //Array should be of the type integer
+                // please find the way to set this mehtod to be the protected member of this class
+                this.counter = 0;
+            for (let i = 0; i < arrayName.length; i++) {
+                this.counter = this.counter + arrayName[i];
+            }
+            
+            return this.counter;
+            }
 
-//             addNewProduct = function(product){
-//                 this.AvailableProducts.push(product);
-//                 this.AvairableNumItems.push(0);
-//                 return true;
-//             }
-//         }
+            addNewProduct = function(product,cost,InitialItems = 1){
+                if ((product !="" && isNaN(product)) && (!isNaN(cost) && cost != 0 )) {
+                    this.AvailableProducts.push(product);
+                    this.productCost.push(cost);
+                    this.AvairableNumItems.push(InitialItems);
+                    this.NumberOfProducts++;
+                    return true;
+                }else{
+                    return false;
+                }
+                                            }
+            
+            pushNewItems = function(ItemIndex,NumberOfItems){
+                this.AvairableNumItems[ItemIndex] = this.AvairableNumItems[ItemIndex] + NumberOfItems;
+                return true;
+            }
+            checkAvailableItems = function(ItemIndex){
+                return this.AvairableNumItems[ItemIndex];
+            }
 
+            popItems = function(ItemIndex,NumberOfItems){
+                if (this.AvairableNumItems[ItemIndex] != 0) {
+                    this.AvairableNumItems[ItemIndex] = this.AvairableNumItems[ItemIndex] - NumberOfItems;
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+
+            updateCost = function(ItemIndex,newCost){
+                this.productCost[ItemIndex] = newCost;
+                return true;
+            }
+
+            totalItemsInStock = function(){
+               return this.arraySummation(this.AvairableNumItems);
+            }
+}
+
+
+
+export {Stock};
